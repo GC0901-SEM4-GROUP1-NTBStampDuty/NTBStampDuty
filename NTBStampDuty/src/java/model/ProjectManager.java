5e3230eb8b8d3474b40bf907c4dd308aae42c4a7
DucPhuc95 <ducphuc9533@yahoo.com>
2015-06-02 16:42:06 +0700

    Add getProjectByStatus method

--- NTBStampDuty/src/java/model/ProjectManager.java
+++ NTBStampDuty/src/java/model/ProjectManager.java
@@ -182,4 +182,59 @@ public class ProjectManager {
         }
         return projectList;
     }
+    
+    public List<Project> getProjectByStatus(String fileterType, int startIndex, int endIndex) {
+        try {
+            GetConnection conn = new GetConnection();
+            PreparedStatement ps = conn.getConnection().prepareStatement(
+                    "WITH limt_project AS\n"
+                    + "(select proj_id, proj_name,tblProjects.building_id as buildingid, building_name, complete_percent, created_date, finish_date, period, ROW_NUMBER() OVER (ORDER BY proj_id ASC) AS [row_number]\n"
+                    + "from tblProjects\n"
+                    + "inner join tblBuildingDetails\n"
+                    + "on tblProjects.building_id = tblBuildingDetails.building_id\n"
+                    + "where available_status = 1 and complete_percent ? ?"
+                    + ")\n"
+                    + "select proj_id, proj_name,buildingid, building_name, complete_percent, created_date, finish_date, period FROM limt_project WHERE [row_number]>" + startIndex + " AND [row_number]<=" + endIndex
+            );
+            switch (fileterType) {
+                case "notBuilt":
+                    ps.setString(1, "=");
+                    ps.setString(2, "0");
+                    break;
+                case "beingBuilt":
+                    ps.setString(1, ">");
+                    ps.setString(2, "0");
+                    break;
+                case "built":
+                    ps.setString(1, "=");
+                    ps.setString(2, "100");
+                    break;
+            }
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Project project = new Project();
+                project.setProjectID(rs.getInt("proj_id"));
+                project.setProjectName(rs.getString("proj_name"));
+                project.setBuildingId(rs.getInt("buildingid"));
+                project.setBuildingName(rs.getString("building_name"));
+                project.setCompletePercent(rs.getInt("complete_percent"));
+                Date createdDate = rs.getDate("created_date");
+                DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+                project.setCreatedDate(dateFormat.format(createdDate));
+                Date finishedDate = rs.getDate("finish_date");
+                project.setFinishDate(dateFormat.format(finishedDate));
+                project.setPeriod(rs.getInt("period"));
+                projectList.add(project);
+            }
+            rs.close();
+            ps = conn.getConnection().prepareStatement("select count(*) from tblProjects");
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                this.noOfRecords = rs.getInt(1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return projectList;
+    }
 }
